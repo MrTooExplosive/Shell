64e5566d65fbec64cd127e68d266cc4b2bda61ba
Makarius Salib <misalib@cpp.edu>
2021-10-22 05:35:09 +0100

    Defines lsh_loop() function

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+void lsh_loop();
+
 int main(int argc, char **argv)
 {
 	// Load config files
@@ -10,3 +12,20 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
+void lsh_loop()
+{
+	char *line;
+	char **args;
+	int status;
+
+	do
+	{
+		printf("> ");
+		line = lshReadLine();
+		args = lshSplitLine(line);
+		status = lshExecute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
